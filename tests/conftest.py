24163a7ae126ab40fc3f8a1e6f8f0b4f1f49cdb5
YudinRomanovich <den.yudin2020@gmail.com>
2024-01-25 22:00:00 +0300

    add docker containers for app and db

---
+++ tests/conftest.py
@@ -0,0 +1,53 @@
+import asyncio
+import pytest
+
+from typing import AsyncGenerator
+from httpx import AsyncClient
+
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import NullPool
+
+from src.database import get_async_session
+from src import metadata
+from src.config import DB_HOST_TEST, DB_NAME_TEST, DB_PASS_TEST, DB_PORT_TEST, DB_USER_TEST
+from src.main import app
+
+
+# DATABASE TEST
+DATABASE_URL_TEST = f"postgresql+asyncpg://{DB_USER_TEST}:{DB_PASS_TEST}@{DB_HOST_TEST}:{DB_PORT_TEST}/{DB_NAME_TEST}"
+
+# CREATE ENGINE
+engine_test = create_async_engine(DATABASE_URL_TEST, poolclass=NullPool)
+async_session_maker = sessionmaker(engine_test, class_=AsyncSession, expire_on_commit=False)
+metadata.bind = engine_test
+
+
+async def override_get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
+
+
+app.dependency_overrides[get_async_session] = override_get_async_session
+
+
+@pytest.fixture(autouse=True, scope="session")
+async def prepare_database():
+    async with engine_test.begin() as conn:
+        await conn.run_sync(metadata.create_all)
+    yield
+    async with engine_test.begin() as conn:
+        await conn.run_sync(metadata.drop_all)
+
+# SETUP
+@pytest.fixture(scope="session")
+def event_loop(request):
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest.fixture(scope="session")
+async def ac() -> AsyncGenerator[AsyncClient, None]:
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        yield ac
---
+++ tests/test_menu.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+# @pytest.mark.asyncio
+
+async def test_one():
+
+    assert 1 == 1
+
+
+async def test_two():
+
+    assert 2 == 2
